2e447cbdfd1137442f74a370c127f1388e60d156
rooboo14 <magicalworldtree@gmail.com>
2019-11-03 13:47:29 -0500

    Initial Commit

---
+++ SpeedRush.Leaderboard/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpeedRush.Leaderboard.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
