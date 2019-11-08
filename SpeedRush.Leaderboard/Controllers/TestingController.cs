9e0c3d208e26676111ae3ba27a38735e0a41f128
rooboo14 <magicalworldtree@gmail.com>
2019-11-07 20:58:42 -0500

    Made "Thing" just to test out the URL stuff

---
+++ SpeedRush.Leaderboard/Controllers/TestingController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using System.Text.Encodings.Web;
+
+namespace SpeedRush.Leaderboard.Controllers
+{
+    public class TestingController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Thing(int? id, string name)
+        {
+            if (id < 1)
+            {
+                return NotFound();
+            }
+            if (id == null)
+            {
+                ViewData["Message"] = "Hello! This is the thing view page. :)";
+                return View();
+            }
+            ViewData["Message"] = "Hello, " + name + "! The id you requested is: " + id;
+            return View();
+        }
+
+    }
+}
